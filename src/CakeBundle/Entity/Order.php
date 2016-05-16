cd864078de9eadc149e241cfe9c7e8ba45297dc7
mocniak <karol@mocniak.pl>
2016-05-17 00:35:56 +0200

    - fixed orm mapping for order

--- src/CakeBundle/Entity/Order.php
+++ src/CakeBundle/Entity/Order.php
@@ -2,12 +2,26 @@
 
 namespace CakeBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="orders")
+ */
 class Order {
 
     /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    
+    private $id;
+    /**
      * @var OrderItemInterface[]
      */
+
     private $orderItems;
 
     /**
@@ -26,5 +40,23 @@ class Order {
     public function getOrderItems()
     {
         return $this->orderItems;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Order
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
     }
 }
