133b4a0a5c4dc723cd2e2cfc63d64e9678327c5e
mocniak <karol@mocniak.pl>
2016-05-25 15:59:32 +0200

    - order admin init service

---
+++ src/CakeBundle/Admin/OrderAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CakeBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+
+class OrderAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('cake', 'entity', array(
+                'label' => 'Cake',
+                'class' => 'CakeBundle\Entity\Cake'
+            ))
+            ->add('porions')
+            ->add('numberOfFloors')
+            ->add('deliveryDate')
+            ->add('phone')
+            ->add('email')
+            ->add('notes');
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name');
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('spongeType')
+            ->add('pricePerPortion')
+            ->add('official')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ));
+    }
+
+    // Fields to be shown on show action
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('title')
+            ->add('pricePerPortion');
+    }
+}
--- src/CakeBundle/Entity/Order.php
+++ src/CakeBundle/Entity/Order.php
@@ -71,29 +71,6 @@ class Order {
      * @var string
      */
     private $notes;
-    
-    /**
-     * @var OrderItemInterface[]
-     */
-    private $orderItems;
-
-    /**
-     * @param OrderItemInterface[] $orderItems
-     * @return Order
-     */
-    public function setOrderItems($orderItems)
-    {
-        $this->orderItems = $orderItems;
-        return $this;
-    }
-
-    /**
-     * @return OrderItemInterface[]
-     */
-    public function getOrderItems()
-    {
-        return $this->orderItems;
-    }
 
     /**
      * @param mixed $id
